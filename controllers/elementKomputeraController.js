982bf7b4a5bd2e042d17c6b94924ff05ba2cd23b
Vladyslav Domariev <s19314@pjwstk.edu.pl>
2020-12-17 16:35:34 +0200

    Реализовал вывод списка элементов Version 4,1

--- controllers/elementKomputeraController.js
+++ controllers/elementKomputeraController.js
@@ -1,5 +1,15 @@
+const ElemetnKomputeraRepository = require('../repository/mysql2/ElementKomputeraRepository');
+
 exports.showElementKomputerList = (req, res, next) => {
-    res.render('pages/element_komputera/computer-element-list', { navLocation: 'elementKomputer' });
+    ElemetnKomputeraRepository.getElements_Komputera()
+        .then(elements => {
+            res.render('pages/element_komputera/computer-element-list',
+                {
+                    elements: elements,
+                    navLocation: 'elementKomputer'
+                });
+        })
+
 }
 
 exports.showAddElementKomputerForm = (req, res, next) => {
