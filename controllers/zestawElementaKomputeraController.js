0e2b974fe5a9b39f50ee1fb939777391b45ada01
Vladyslav Domariev <s19314@pjwstk.edu.pl>
2021-01-05 14:09:23 +0200

    Zestaw Edit генерирует(в Controller была неверная ссылка), но не работает отправка/подтверждение: Usun, Add nowy, edytuj. Начинаю решать вопрос с POSTом Version 4.14

--- controllers/zestawElementaKomputeraController.js
+++ controllers/zestawElementaKomputeraController.js
@@ -52,11 +52,10 @@ exports.showEditZestawElementKomputerForm = (req, res, next) => {
             return ZestawElementaKomputeraRepository.getZestawElementaKomputeraById(zestawId);
         })
         .then(z_e_k => {
-            res.render('pages/zestaw_elementa_i_komputera/form_reserve_copy/form_special_for_edit', { // 'form_special_for_edit'  // form
-                /*    
+            res.render('pages/zestaw_elementa_i_komputera/form', { // 'form_special_for_edit'  // form
                 allZestaws: allZestaws,
-                    allKomputers: allKomputers,
-                    allElements: allElements,*/
+                allKomputers: allKomputers,
+                allElements: allElements,
                 zestaw_elementa_i_komputera: z_e_k,
                 pageTitle: 'Edycja zestaw z elementa i komputera',
                 formMode: 'edit',
